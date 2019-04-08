f0a020e540db94246d0737cffa1b722983e4ec4e
David Acosta <davidacosta25081@yahoo.com>
2019-04-08 23:32:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,8 +111,9 @@ def self.first_student_in_grade_10
     LIMIT 1
   SQL
 
-  student = DB[:conn].execute(sql).flatten
-  student
+  student = DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end.first
 
 end
 
